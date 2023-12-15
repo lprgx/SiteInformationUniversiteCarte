f493cebd9a535cfdc9c5d73d73daa3519356c546
louis.prigneaux <louis.prigneaux@epita.fr>
2023-12-15 10:42:19 +0100

    Base du site

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([15.92089137885494, -61.637999775238015], 10);
+var map = L.map('map').setView([48.866667, 2.333333], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 12,
-    minZoom: 9,
+    maxZoom: 30,
+    minZoom: 7,
     //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
@@ -20,7 +20,7 @@ xmlhttp.onreadystatechange = function () {
                     "fillOpacity": 0.2
                 },
                 onEachFeature: function(feature, layer){
-                    layer.bindPopup(feature.properties.nom);
+                    layer.bindPopup(feature.properties.name);
                     layer.on('mouseover', function(e){
                         this.openPopup()
                     })
